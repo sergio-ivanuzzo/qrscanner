dc4793a04859dbf2c2d4e35058d221f16d0c85f5
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2018-11-07 15:45:56 +0200

    restyled

--- qr_scanner.js
+++ qr_scanner.js
@@ -21,7 +21,6 @@ class QRScanner {
             this.qrCode = jsQR(imageData, this.captureArea.width, this.captureArea.height);
 
             if (this.qrCode) {
-                alert('QR code was detected');
                 router.redirect(Router.pages.result, this.qrCode);
                 let resultPage = document.querySelector('#result .qr-code-data');
                 resultPage.innerHTML = this.qrCode.data;
